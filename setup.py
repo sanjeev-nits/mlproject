1156330c4e3dbc44514768b649d15bc45307b63f
sanjeev <sanjeevkumar814155@gmail.com>
2025-06-21 19:10:04 +0530

    req

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages
+from typing import List
+
+
+def get_requirements(file_path:str)-> List[str]:
+    """this funtion will return list of requirements"""
+    
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+    
+
+
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Sanjeev',
+    author_email='sanjeevkumar814155@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
