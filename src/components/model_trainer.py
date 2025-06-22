b104dd5e01345211594f3b2ff78ea78215f4e858
sanjeev <sanjeevkumar814155@gmail.com>
2025-06-22 21:34:07 +0530

    hyperparameter

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,61 +1,88 @@
-import numpy as np
-import pandas as pd
 import os
 import sys
 from dataclasses import dataclass
-# Modelling
 
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor
-from sklearn.svm import SVR
-from sklearn.linear_model import LinearRegression, Ridge,Lasso
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-from sklearn.model_selection import RandomizedSearchCV
-from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
-import warnings
-warnings.filterwarnings("ignore")
+
+from src.exception import CustomException
+from src.logger import logging
 
 from src.utils import save_object,evaluate_models
-from src.logger import logging
-from src.exception import CustomException
-import numpy as np
-
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts", "model.pkl")
-    
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
+
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
-    
-    def initiate_model_trainer(self, train_array, test_array):
+        self.model_trainer_config=ModelTrainerConfig()
+
+
+    def initiate_model_trainer(self,train_array,test_array):
         try:
-            # split the features and target
-            logging.info("split training and test arry")
+            logging.info("Split training and test input data")
             x_train = train_array[:, :-1]
             y_train = train_array[:, -1]
             x_test = test_array[:, :-1]
             y_test = test_array[:, -1]
-
-            
-            models={
-                'Random Forest':RandomForestRegressor(),
-                'Decision Tree':DecisionTreeRegressor(),
-                'Gradient Boosting':GradientBoostingRegressor(),
-                'XGBoost':XGBRegressor(),
-                'CatBoost':CatBoostRegressor(),
-                'AdaBoost':AdaBoostRegressor(),
-                'Linear Regression':LinearRegression(),
-                'Ridge':Ridge(),
-                'Lasso':Lasso(),
-                'SVR':SVR(),
-                'KNN':KNeighborsRegressor() 
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
             
-            model_report:dict = evaluate_models(x_train, y_train, x_test, y_test, models)
+            model_report:dict = evaluate_models(x_train, y_train, x_test, y_test, models,params)
             
             best_model_score=max(sorted(model_report.values()))
             
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -20,11 +21,16 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            param=params[list(models.keys())[i]]
+            gs=GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
             
             y_train_pred = model.predict(X_train)
